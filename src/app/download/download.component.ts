480c7d6cfa4ccb500e1f04754eabe63e70fca899
Absolute Broking <project@absolutebroking.com>
2026-06-08 13:12:09 +0530

    Update download.component.ts

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -1,11 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-download',
-  imports: [],
-  templateUrl: './download.component.html',
-  styleUrl: './download.component.css'
-})
-export class DownloadComponent {
-
-}
